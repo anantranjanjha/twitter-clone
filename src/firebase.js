bea1cdc24abc67ba16d15c4be604ca88f0549047
anantranjanjha <anantranjanjha@gmail.com>
2022-10-04 15:02:01 +0530

    initial commit

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore"
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBg76QLqsNRM0QKz_WWhHNnligpfZlD7dA",
-    authDomain: "twitter-clone-59ee6.firebaseapp.com",
-    projectId: "twitter-clone-59ee6",
-    storageBucket: "twitter-clone-59ee6.appspot.com",
-    messagingSenderId: "921036174478",
-    appId: "1:921036174478:web:d82ab5ae3fa82f8b5f05df"
+
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
